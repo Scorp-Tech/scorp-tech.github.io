a5f79834caa304362c70f1fa9411246de2cfea0e
Sam <omprakash121samuni@gmail.com>
2022-11-13 16:34:40 +0530

    Sam_commit_check

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // change icon based on the theme
-if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
     document.getElementById('iconFevicon').href = './Assets/Icons/Scorp_Favicon_Light.png';
-}else{
+    document.getElementById('themeStyle').href = './Assets/Style/scorp_dark.css';
+} else {
     document.getElementById('iconFevicon').href = './Assets/Icons/Scorp_Favicon_Dark.png';
+    document.getElementById('themeStyle').href = './Assets/Style/scorp_light.css';
 }
